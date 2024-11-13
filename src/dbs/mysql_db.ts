1278ce4a43d41d0acab5a71cdba68f032caf0c36
Rocksen1683 <edpravee@uwaterloo.ca>
2024-11-12 22:01:24 -0500

    added logger

--- src/dbs/mysql_db.ts
+++ src/dbs/mysql_db.ts
@@ -3,9 +3,11 @@ import { IDatabase } from "../interfaces";
 import { Category, Order, User, UserPatchRequest } from "../types";
 import { v4 as uuidv4 } from 'uuid'; 
 import mysql from "mysql2/promise";
+import logger  from "../logger";
 
 export default class MySqlDB implements IDatabase {
   connection: mysql.Connection;
+  static requestCount = 0;
 
   async init() {
     this.connection = await mysql.createConnection({
@@ -15,22 +17,30 @@ export default class MySqlDB implements IDatabase {
       port: parseInt(process.env.RDS_PORT), // Default to 3306 if port is not specified
       database: process.env.RDS_DATABASE,
     });
-    console.log("MySQL connected!");
+    logger.info("MySQL connected!");
   }
 
   constructor() {
     this.init();
   }
 
+  private logRequest(endpoint: string, params?: any) {
+    MySqlDB.requestCount++;
+    logger.info(`Request to ${endpoint}. Parameters: ${JSON.stringify(params)}. Total requests: ${MySqlDB.requestCount}`);
+  }
+
   async queryProductById(productId: string) {
+    this.logRequest("queryProductById", { productId });
     return (await this.connection.query(`SELECT * FROM products WHERE id = ?`, [productId]))[0][0] as Product;
   }
 
   async queryRandomProduct() {
+    this.logRequest("queryRandomProduct");
     return (await this.connection.query(`SELECT * FROM products ORDER BY RAND() LIMIT 1;`))[0][0] as Product;
   }
 
   queryAllProducts = async (categoryId?: string) => {
+    this.logRequest("queryAllProducts", { categoryId });
     if (categoryId) {
       return (await this.connection.query(`SELECT * FROM products WHERE categoryId = ?`, [categoryId]))[0] as Product[];
     } else {
@@ -39,30 +49,37 @@ export default class MySqlDB implements IDatabase {
   }
 
   queryAllCategories = async () => {
+    this.logRequest("queryAllCategories");
     return (await this.connection.query("SELECT * FROM categories;"))[0] as Category[];
   }
 
   queryAllOrders = async () => {
+    this.logRequest("queryAllOrders");
     return (await this.connection.query("SELECT * FROM orders;"))[0] as Order[];
   }
 
   async queryOrdersByUser(userId: string) {
+    this.logRequest("queryOrdersByUser", { userId });
     return (await this.connection.query(`SELECT * FROM orders WHERE userId = ?`, [userId]))[0] as Order[];
   }
 
   queryOrderById = async (id: string) => {
+    this.logRequest("queryOrderById", { id });
     return (await this.connection.query(`SELECT * FROM orders WHERE id = ?`, [id]))[0][0];
   }
 
   queryUserById = async (id: string) => {
+    this.logRequest("queryUserById", { id });
     return (await this.connection.query(`SELECT id, email, name FROM users WHERE id = ?`, [id]))[0][0];
   }
 
   queryAllUsers = async () => {
+    this.logRequest("queryAllUsers");
     return (await this.connection.query("SELECT id, name, email FROM users"))[0] as User[];
   }
 
   insertOrder = async (order: Order) => {
+    this.logRequest("insertOrder", { orderId: order.id, userId: order.userId });
     const { id, userId, totalAmount } = order;
     await this.connection.query(
       `INSERT INTO orders (id, userId, totalAmount) VALUES (?, ?, ?)`,
@@ -79,6 +96,7 @@ export default class MySqlDB implements IDatabase {
   }
   
   updateUser = async (patch: UserPatchRequest) => {
+    this.logRequest("updateUser", { patch });
     const fields: string[] = [];
     const values: any[] = [];
   
@@ -102,6 +120,7 @@ export default class MySqlDB implements IDatabase {
   
 
   deleteOrder = async (id: string) => {
+    this.logRequest("deleteOrder", { id });
     await this.connection.query(`DELETE FROM order_items WHERE orderId = ?`, [id]);
     await this.connection.query(`DELETE FROM orders WHERE id = ?`, [id]);
   }
--- src/servers/restServer.ts
+++ src/servers/restServer.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import { AllProductsRequest, Order, OrderRequest, ProductRequest, UserPatchRequest, UserRequest } from "../types";
+import logger from "../logger";
 
 export default class RestServer implements IServer {
 
@@ -122,7 +123,7 @@ export default class RestServer implements IServer {
     }); // Deletes an order by id
 
     this.server.listen(port, () => {
-      console.log(`REST server listening on port ${port}`);
+      logger.info(`REST server listening on port ${port}`);
     });
   }
 }
